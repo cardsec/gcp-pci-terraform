48088f7c05ecb2ba7500ea0f44e7fe15a5cf5fef
anners <anners@google.com>
2018-08-21 13:51:08 -0500

    removed unused comments

--- pci-sql.tf
+++ pci-sql.tf
@@ -12,10 +12,6 @@ resource "google_sql_database_instance" "pci_master" {
     ip_configuration {
       ipv4_enabled = true
       # require_ssl = true
-      #authorized_networks = {
-      #  name = "The World"
-      #  value = "0.0.0.0/0"
-      #}
       authorized_networks = {
         name = "${google_compute_instance.pci_web.0.name}"
         value = "${google_compute_address.pci_web.address}/32"
@@ -24,9 +20,3 @@ resource "google_sql_database_instance" "pci_master" {
   }
   depends_on = ["google_compute_instance.pci_web", "google_project_services.in_scope_cde"]
 }
-
-#resource "google_sql_database" "pci_cc" {
-#  name      = "users-cc"
-#  instance  = "${google_sql_database_instance.pci_master.name}"
-#  depends_on = ["google_sql_database_instance.pci_master"]
-#}

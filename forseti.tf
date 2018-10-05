c48045d39bf9fa3f17b5680f4dfbfccf0d778080
anners <anners@google.com>
2018-10-05 15:12:06 -0700

    adding forseti to the diagram

--- forseti.tf
+++ forseti.tf
@@ -6,6 +6,7 @@
    gsuite_admin_email           = "susan@girlsgonewildwood.com"
    project_id                   = "${google_project.forseti.project_id}"
    download_forseti             = "true"
+   forseti_repo_branch          = "dev"
    notification_recipient_email = "ann@girlsgonewildwood.com"
    #credentials_file_path        = "${base64decode(google_service_account_key.forseti.private_key)}"
    credentials_file_path        = "/tmp/forseti.json"

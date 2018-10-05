3df8aedc564845c1fd33a729a2e764c9adf25afa
anners <anners@google.com>
2018-10-05 11:29:20 -0700

    adding forseti info

--- forseti-project.tf
+++ forseti-project.tf
@@ -12,6 +12,16 @@ resource "google_project_services" "forseti" {
   services = [
     "compute.googleapis.com",
     "oslogin.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "admin.googleapis.com",
+    "bigquery-json.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "sql-component.googleapis.com",
+    "sqladmin.googleapis.com",
+    "deploymentmanager.googleapis.com",
+    "iam.googleapis.com",
   ]
 }
 
@@ -35,10 +45,84 @@ resource "google_service_account_key" "forseti" {
   service_account_id = "${google_service_account.forseti.name}"
 }
 
-resource "google_organization_iam_binding" "resourcemgr_org_admin" {
+#Roles Granted at the organization level
+resource "google_organization_iam_binding" "appengine" {
   org_id = "${var.org_id}"
-  role    = "roles/resourcemanager.organizationAdmin"
+  role    = "roles/appengine.appViewer"
   members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "bq" {
+  org_id = "${var.org_id}"
+  role    = "roles/bigquery.dataViewer"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "browser" {
+  org_id = "${var.org_id}"
+  role    = "roles/browser"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "cloudsql" {
+  org_id = "${var.org_id}"
+  role    = "roles/cloudsql.viewer"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "compute_network" {
+  org_id = "${var.org_id}"
+  role    = "roles/compute.networkViewer"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "compute_security" {
+  org_id = "${var.org_id}"
+  role    = "roles/compute.securityAdmin"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "iam" {
+  org_id = "${var.org_id}"
+  role    = "roles/iam.securityReviewer"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "servicemanagement" {
+  org_id = "${var.org_id}"
+  role    = "roles/servicemanagement.quotaViewer"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+resource "google_organization_iam_binding" "serviceusage" {
+  org_id = "${var.org_id}"
+  role    = "roles/serviceusage.serviceUsageConsumer"
+  members = ["serviceAccount:${google_service_account.forseti.email}"]
+}
+
+#Granted at the project level
+
+resource "google_project_iam_binding" "cloudsql" {
+  project = "${google_project.forseti.project_id}"
+  role = "roles/cloudsql.client"
+  members = [ "serviceAccount:${google_service_account.forseti.email}" ]
+}
+
+resource "google_project_iam_binding" "logging" {
+  project = "${google_project.forseti.project_id}"
+  role = "roles/logging.logWriter"
+  members = [ "serviceAccount:${google_service_account.forseti.email}" ]
+}
+
+resource "google_project_iam_binding" "storage" {
+  project = "${google_project.forseti.project_id}"
+  role = "roles/storage.objectViewer"
+  members = [ "serviceAccount:${google_service_account.forseti.email}" ]
+}
+resource "google_project_iam_binding" "storage_creator" {
+  project = "${google_project.forseti.project_id}"
+  role = "roles/storage.objectCreator"
+  members = [ "serviceAccount:${google_service_account.forseti.email}" ]
 }
 
 resource "google_project_iam_binding" "compute_admin" {
@@ -86,6 +170,13 @@ resource "google_project_iam_binding" "serviceusage_admin" {
 resource "google_project_iam_binding" "storage_admin" {
   project = "${google_project.forseti.project_id}"
   role = "roles/storage.admin"
+  members = [ "serviceAccount:${google_service_account.forseti.email}" ]
+}
+
+#Granted at the service account level
+resource "google_service_account_iam_binding" "admin-account-iam" {
+  service_account_id = "projects/${google_project.forseti.project_id}/serviceAccounts/${google_service_account.forseti.email}"
+  role        = "roles/iam.serviceAccountTokenCreator"
   members = [ "serviceAccount:${google_service_account.forseti.email}" ]
 }
 
---
+++ forseti.tf
@@ -0,0 +1,12 @@
+/******************************************
+   Forseti Module Install
+  *****************************************/
+ module "forseti-install-simple" {
+   source                       = "github.com/terraform-google-modules/terraform-google-forseti"
+   gsuite_admin_email           = "susan@girlsgonewildwood.com"
+   project_id                   = "${google_project.forseti.project_id}"
+   download_forseti             = "true"
+   notification_recipient_email = "ann@girlsgonewildwood.com"
+   #credentials_file_path        = "${base64decode(google_service_account_key.forseti.private_key)}"
+   credentials_file_path        = "/tmp/forseti.json"
+ }

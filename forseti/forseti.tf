022e80f2035e48ec07e03618ee446bbf3c74dc74
anners <anners@google.com>
2018-11-02 12:14:25 -0400

    added license

--- forseti/forseti.tf
+++ forseti/forseti.tf
@@ -1,13 +1,27 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /******************************************
    Forseti Module Install
   *****************************************/
  module "forseti-install-simple" {
    source                       = "github.com/terraform-google-modules/terraform-google-forseti"
-   gsuite_admin_email           = "anners@google.com"
-   project_id                   = "forseti-17a65e374c80b8b1"
+   gsuite_admin_email           = ""
+   project_id                   = ""
    download_forseti             = "true"
    forseti_repo_branch          = "dev"
-   notification_recipient_email = "anners@google.com"
-   #credentials_file_path        = "${base64decode(google_service_account_key.forseti.private_key)}"
+   notification_recipient_email = ""
    credentials_file_path        = "/tmp/key.json"
  }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 provider "google" {
   region = "${var.region}"
 }
@@ -16,17 +31,16 @@ variable "region_zone" {
 
 variable "org_id" {
   description = "The ID of the Google Cloud Organization."
-  default     = "870160332389"
+  default     = ""
 }
 
 variable "billing_account" {
   description = "The ID of the associated billing account (optional)."
-#  default     = "01A865-FEFFAE-D9C1D9"
-  default     = "01D7D9-872112-4023F1"
+  default     = ""
 }
 
 
 variable "credentials_file_path" {
   description = "Location of the credentials to use."
-  default     = "/Users/anners/.config/gcloud/anners-demo-terraform-admin.json"
+  default     = ""
 }
